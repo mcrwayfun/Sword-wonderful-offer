55d21445ae42a8babd87b6277d2bed1b5ce154b7
mcrwayfun <mcrwayfun@gmail.com>
2018-12-28 19:19:45 +0800

    NumericStrings

---
+++ src/_20_01_NumericStrings/Solution.java
@@ -0,0 +1,21 @@
+package _20_01_NumericStrings;
+
+/**
+ * @author mcrwayfun
+ * @version 1.0
+ * @description
+ * @date Created in 2018/12/28
+ */
+public class Solution {
+
+    public boolean isNumeric(char[] str) {
+        if (str == null || str.length == 0) {
+            return false;
+        }
+
+        // 数字格式可以使用A[.[B]][e|EC]或者.B[e|EC]
+
+
+        return false;
+    }
+}
