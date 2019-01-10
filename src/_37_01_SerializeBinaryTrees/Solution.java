32b39b3affd68bc073aca72496da1259aec61f23
mcrwayfun <mcrwayfun@gmail.com>
2019-01-10 23:51:54 +0800

    no test version

--- src/_37_01_SerializeBinaryTrees/Solution.java
+++ src/_37_01_SerializeBinaryTrees/Solution.java
@@ -22,12 +22,38 @@ public class Solution {
 
         res.append(root.val);
         res.append(",");
-        res.append(Serialize())
+        res.append(Serialize(root.left));
+        res.append(Serialize(root.right));
 
+        return res.toString();
     }
 
 
     public TreeNode Deserialize(String str) {
 
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+
+
+        String[] strr = str.split(",");
+
+
+        return deserializeHelper(strr, -1);
+
+    }
+
+    private TreeNode deserializeHelper(String[] strr, int index) {
+
+        index++;
+
+        TreeNode node = null;
+        if (!"#".equals(strr[index])) {
+            node = new TreeNode(Integer.valueOf(strr[index]));
+            node.left = deserializeHelper(strr, index);
+            node.right = deserializeHelper(strr, index);
+        }
+
+        return node;
     }
 }
