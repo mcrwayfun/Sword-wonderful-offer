d8e8ae2036c846cf1206cc5690993179fb373411
mcrwayfun <mcrwayfun@gmail.com>
2019-01-12 10:32:22 +0800

    fix bug

--- src/_37_01_SerializeBinaryTrees/Solution.java
+++ src/_37_01_SerializeBinaryTrees/Solution.java
@@ -1,6 +1,7 @@
 package _37_01_SerializeBinaryTrees;
 
 import pojo.TreeNode;
+import utils.TreeNodeUtil;
 
 import java.io.Serializable;
 
@@ -12,36 +13,81 @@ import java.io.Serializable;
  */
 public class Solution {
 
+
     public String Serialize(TreeNode root) {
 
         StringBuilder res = new StringBuilder();
         if (root == null) {
-            res.append("#,");
             return res.toString();
+        }
+
+        serializeHelper(root, res);
+        // 移除最后一个的符号","
+        res.deleteCharAt(res.lastIndexOf(","));
+        return res.toString();
+    }
+
+    private void serializeHelper(TreeNode root, StringBuilder res) {
+
+        if (root == null) {
+            res.append("#");
+            res.append(",");
+            return;
         }
 
         res.append(root.val);
         res.append(",");
-        res.append(Serialize(root.left));
-        res.append(Serialize(root.right));
-
-        return res.toString();
+        serializeHelper(root.left, res);
+        serializeHelper(root.right, res);
     }
-
 
     private int index = -1;
 
-    TreeNode Deserialize(String str) {
+    public TreeNode Deserialize(String str) {
+
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+
+        String[] treeNodeStr = str.split(",");
+        return deserializeHelper(treeNodeStr);
+    }
+
+    private TreeNode deserializeHelper(String[] treeNodeStr) {
+
         index++;
-        
-        String[] strr = str.split(",");
         TreeNode node = null;
-        if (!strr[index].equals("#")) {
-            node = new TreeNode(Integer.valueOf(strr[index]));
-            node.left = Deserialize(str);
-            node.right = Deserialize(str);
+
+        // index不越界并且当前节点不为#
+        if (index < treeNodeStr.length && !"#".equals(treeNodeStr[index])) {
+            node = new TreeNode(Integer.valueOf(treeNodeStr[index]));
+            node.left = deserializeHelper(treeNodeStr);
+            node.right = deserializeHelper(treeNodeStr);
         }
 
         return node;
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        // 反序列化
+        String str = "1,2,4,#,#,#,3,5,#,#,6,#,#";
+
+        /*
+         *     			1
+         *           2	 	3
+         *       4	  #	 5		6
+         *   #	  #	   #   #  #   #
+         */
+        TreeNode root = solution.Deserialize(str);
+
+        System.out.println("生成的二叉树前序遍历为" + TreeNodeUtil.preOrder(root));
+
+        // 序列化
+        String serialize = solution.Serialize(root);
+        System.out.println("序列化结果为：" + serialize);
+
     }
 }
