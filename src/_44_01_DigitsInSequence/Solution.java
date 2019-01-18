ecc75cd2804d20f85cdf3552d0761a935e61c621
mcrwayfun <mcrwayfun@gmail.com>
2019-01-19 00:18:24 +0800

    add explanation

--- src/_44_01_DigitsInSequence/Solution.java
+++ src/_44_01_DigitsInSequence/Solution.java
@@ -12,17 +12,26 @@ public class Solution {
         if (n < 0) {
             return -1;
         }
+
         int digits = 1;
         while (true) {
+            // 根据位数判断当前是前几位
+            // 比如1则是前10位，因为有0~9这10个数字，所以1*10=10
+            // 比如2则是前180位，因为有10~99这90个数字，所以2*90=180
             long numbers = countOfIntegers(digits);
+
+            // n是否在当前位数中
             if (n < digits * numbers) {
                 break;
             }
-            n -= numbers * digits;
-            ++digits;
-        }
-        return digitAtIndex(digits, n);
 
+            // 不在则n减去相应的位数
+            n -= numbers * digits;
+            digits++;
+        }
+
+
+        return digitAtIndex(digits, n);
     }
 
     private long countOfIntegers(int digits) {
@@ -30,9 +39,15 @@ public class Solution {
     }
 
     private int digitAtIndex(int digits, int n) {
+        // 获取m位数的第一个数字
+        // 比如1位第一个数字是0，2位第一个数字是10
         int beginNumber = getBeginNumber(digits);
+        // 获取这是哪个数，比如求第1001对应的数字，则此时n=811，digits=3,n/digits=270
+        // val = 100+270=370，即第1001对应的那个数是370
         int val = beginNumber + n / digits;
+        // 第1001位处于370的哪一位
         int indexFromRight = digits - n % digits;
+        // 求出位数
         for (int i = 1; i < indexFromRight; ++i) {
             val /= 10;
         }
@@ -43,4 +58,7 @@ public class Solution {
         return digits == 1 ? 0 : (int) Math.pow(10, digits - 1);
     }
 
+    public static void main(String[] args) {
+        new Solution().digitAtIndex(1001);
+    }
 }
