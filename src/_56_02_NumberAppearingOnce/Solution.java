703d341c8c500ed056cdd632f5f0884e749701df
mcrwayfun <mcrwayfun@gmail.com>
2019-02-02 00:41:10 +0800

    another method

--- src/_56_02_NumberAppearingOnce/Solution.java
+++ src/_56_02_NumberAppearingOnce/Solution.java
@@ -9,28 +9,37 @@ package _56_02_NumberAppearingOnce;
 public class Solution {
 
     public int findNumberAppearingOnce(int[] nums) {
+
         if (nums == null || nums.length == 0) {
-            return 0;
+            throw new IllegalArgumentException();
         }
 
-        int[] bits = new int[32];
-        int n = nums.length;
+        // 存储每个数对应的位数和
+        int[] bitSum = new int[32];
+        for (int i = 0; i < nums.length; i++) {
 
-        for (int i = 0; i < n; ++i) {
-            int val = nums[i];
-            for (int j = 0; j < 32; ++j) {
-                bits[j] += (val & 1);
-                val = val >> 1;
+            int value = nums[i];
+            for (int j = 0; j < 32; j++) {
+                // 获取value每一位数，并求和
+                bitSum[j] += value & 1;
+                value = value >> 1;
             }
         }
 
-        int res = 0;
-        for (int i = 0; i < 32; ++i) {
-            if (bits[i] % 3 != 0) {
-                res += Math.pow(2, i);
+        int retValue = 0;
+        for (int i = 0; i < 32; i++) {
+            // 如果不能被3整除，说明当出现一次的数字的对应位数为1
+            if (bitSum[i] % 3 != 0) {
+                retValue += Math.pow(2, i);
             }
         }
 
-        return res;
+        return retValue;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 2, 2, 3, 3, 3};
+        int numberAppearingOnce = new Solution().findNumberAppearingOnce(nums);
+        System.out.println(numberAppearingOnce);
     }
 }
